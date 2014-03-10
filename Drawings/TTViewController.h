ebf9d071ae9b8ba6de19e8f37ee31806c57203bd
sreshetnyak <reshetniak.sergey@gmail.com>
2014-03-10 21:21:07 +0200

    Initial commit from local git

---
+++ Drawings/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  Drawings
+//
+//  Created by sergey on 3/10/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
---
+++ Drawings/TTViewController.m
@@ -0,0 +1,29 @@
+//
+//  TTViewController.m
+//  Drawings
+//
+//  Created by sergey on 3/10/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
